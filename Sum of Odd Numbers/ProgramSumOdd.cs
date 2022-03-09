b3fbbd0cfc2b52839161b04716678f51a9551d4a
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-09 12:05:15 +0200

    summ odd nums

---
+++ Sum of Odd Numbers/ProgramSumOdd.cs
@@ -0,0 +1,18 @@
+﻿var n = int.Parse(Console.ReadLine());
+var counter = 0;
+var result = 0;
+
+for (int i = 1; i < int.MaxValue; i++)
+{
+    if (counter == n)
+    {
+        break;
+    }
+    if (i % 2 != 0)
+    {
+        counter++;
+        result += i;
+        Console.WriteLine(i);
+    }
+}
+Console.WriteLine($"Sum: {result}");
