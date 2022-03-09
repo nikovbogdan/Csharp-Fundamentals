31c13fe529070a7c4d8e8096baa7475180adc9b9
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-09 12:24:04 +0200

    Multiplication 2

---
+++ Multiplication 2.0/ProgramMultiplication2.cs
@@ -0,0 +1,16 @@
+﻿var n = int.Parse(Console.ReadLine());
+var b = int.Parse(Console.ReadLine());
+
+
+if (b > 10)
+{
+    Console.WriteLine($"{n} X {b} = {n*b}");
+}
+else
+{
+    for (int i = b; i < 11; i++)
+    {
+        Console.WriteLine($"{n} X {i} = {n * i}");
+    }
+}
+
