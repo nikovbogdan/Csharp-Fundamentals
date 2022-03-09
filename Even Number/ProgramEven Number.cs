adef4ac1b04550cab32491221f2398393b691c40
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-09 12:30:02 +0200

    Even NUm

---
+++ Even Number/ProgramEven Number.cs
@@ -0,0 +1,15 @@
+﻿var i = int.Parse(Console.ReadLine());
+
+while(true)
+{
+    if (Math.Abs(i) % 2 == 0)
+    {
+        Console.WriteLine($"The number is: {Math.Abs(i)}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please write even number.");
+    }
+    i = int.Parse(Console.ReadLine());
+}
