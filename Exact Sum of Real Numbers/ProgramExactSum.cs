f9ee42e483929303327e5d01a2ea53153b099478
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-10 15:50:29 +0200

    Extract sum of real nums

---
+++ Exact Sum of Real Numbers/ProgramExactSum.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+var input = int.Parse(Console.ReadLine());
+decimal sum = 0;
+
+for (int i = 0; i < input; i++)
+{
+    decimal num = decimal.Parse(Console.ReadLine());
+    sum += num;
+
+}
+Console.WriteLine(sum);
