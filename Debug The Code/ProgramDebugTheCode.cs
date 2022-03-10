67a0aabb24320226fa024b00cc8bc3f1d194c0b3
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-10 15:28:45 +0200

    debug code solution

---
+++ Debug The Code/ProgramDebugTheCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+var startDate = DateTime.ParseExact(Console.ReadLine(),
+            "d.M.yyyy", CultureInfo.InvariantCulture);
+var endDate = DateTime.ParseExact(Console.ReadLine(),
+    "d.M.yyyy", CultureInfo.InvariantCulture);
+var holidaysCount = 0;
+for (var date = startDate; date <= endDate; date = date.AddDays(1))
+    if (date.DayOfWeek == DayOfWeek.Saturday ||
+        date.DayOfWeek == DayOfWeek.Sunday)
+    {
+        holidaysCount++;
+    }
+
+Console.WriteLine(holidaysCount);
+
+/*
+ * d.M.yyyy instead of dd.m.yyy
+ * in if statemeng needs to be || instead of &&
+ * date = date.AddDays(1)
+ * holidaysCount++ in {} 
+ */
