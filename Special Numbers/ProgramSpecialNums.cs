8653dd8c4ad68c1dbc830acff97bc0b7f6df86d5
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-10 16:11:03 +0200

    Special nums

---
+++ Special Numbers/ProgramSpecialNums.cs
@@ -0,0 +1,21 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % 11 == 0)
+    {
+        Console.WriteLine(i + " -> True");
+    }
+    else if (i % 7 == 0)
+    {
+        Console.WriteLine(i + " -> True");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(i + " -> True");
+    }
+    else
+    {
+        Console.WriteLine(i + " -> False");
+    }
+}
