89009a9cbdd0c00659cc03c9d9d362f2e02010f2
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-11 12:18:27 +0200

    TownInfo

---
+++ Town Info/ProgramTownInfo.cs
@@ -0,0 +1,6 @@
+﻿string town = Console.ReadLine();
+string population = Console.ReadLine();
+string area = Console.ReadLine();
+
+Console.WriteLine($"Town {town} has population of {population} and area {area} square km");
+
