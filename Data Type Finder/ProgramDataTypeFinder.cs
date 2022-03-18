451e9545685c9ec5b66bb34ebb5f14302350f35d
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-18 16:34:27 +0200

    Data type finder

---
+++ Data Type Finder/ProgramDataTypeFinder.cs
@@ -0,0 +1,37 @@
+﻿while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "END")
+    {
+        break;
+    }
+
+    bool integerChek = int.TryParse(input, out int integer);
+    bool doubleCheck = double.TryParse(input, out double floating);
+    bool charCheck = char.TryParse(input, out char mychar);
+    bool booleanCheck = bool.TryParse(input, out bool boolean);
+
+    if (integerChek)
+    {
+        Console.WriteLine($"{input} is integer type");
+    }
+    else if (doubleCheck)
+    {
+        Console.WriteLine($"{input} is double type");
+    }
+    else if (charCheck)
+    {
+        Console.WriteLine($"{input} is char type");
+    }
+    else if (booleanCheck)
+    {
+        Console.WriteLine($"{ input} is boolean type");
+    }
+    else
+    {
+        Console.WriteLine($"{input} is string type");
+    }
+
+
+}
