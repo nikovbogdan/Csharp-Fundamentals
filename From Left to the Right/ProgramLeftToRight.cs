c2be8abf4e15b4787ef4ec8187d1df78e17a43e5
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-03-21 13:29:39 +0200

    From Left to Right solution

---
+++ From Left to the Right/ProgramLeftToRight.cs
@@ -0,0 +1,35 @@
+﻿int input = int.Parse(Console.ReadLine());
+long result = 0;
+for (int i = 1; i <= input; i++)
+{
+    string[] numbers = Console.ReadLine().Split(" ");
+
+    long a = long.Parse(numbers[0]);
+    long b = long.Parse(numbers[1]);
+
+
+    if (a > b)
+    {
+        result = 0;
+        while (a != 0)
+        {
+            result += a % 10;
+            a /= 10;
+        }
+        Console.WriteLine(result);
+    }
+    else
+    {
+        result = 0;
+        while (b != 0)
+        {
+            result += b % 10;
+            b /= 10;
+        }
+        Console.WriteLine(result);
+    }
+
+
+
+
+}
