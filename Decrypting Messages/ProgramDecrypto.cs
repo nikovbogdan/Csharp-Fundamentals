763f0067ae19e91268cbc6c136e00058c2dd007f
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-04-01 18:51:04 +0300

    Decrypting Messages

---
+++ Decrypting Messages/ProgramDecrypto.cs
@@ -0,0 +1,9 @@
+﻿int key = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+string message = "";
+
+for (int i = 0; i < n; i++)
+{
+    message += ((char)(char.Parse(Console.ReadLine()) + key)).ToString();
+}
+Console.WriteLine(message);
