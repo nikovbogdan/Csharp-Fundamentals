e96b3c74228370042244864156ae42d257511e99
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-04-01 17:55:38 +0300

    Floating Equality solution

---
+++ Floating Equality/ProgramFloatingEQ.cs
@@ -0,0 +1,12 @@
+﻿double first = Double.Parse(Console.ReadLine());
+double second = Double.Parse(Console.ReadLine());
+if ((Math.Abs(first - second) <= 0.000001))
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+
