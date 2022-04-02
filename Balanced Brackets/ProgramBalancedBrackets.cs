99c2f0f4a9aa87ed605350c82a74a50953d66d75
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-04-02 16:27:46 +0300

    Balanced Brackets

---
+++ Balanced Brackets/ProgramBalancedBrackets.cs
@@ -0,0 +1,39 @@
+﻿int n = int.Parse(Console.ReadLine());
+int counter = 0;
+string token = "";
+
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine().Trim();
+
+    if (input == "(")
+    {
+        if (token == input)
+        {
+            Console.WriteLine("UNBALANCED");
+            return;
+        }
+        token = input;
+        counter++;
+    }
+
+    if (input == ")")
+    {
+        if (counter == 0)
+        {
+            Console.WriteLine("UNBALANCED");
+            return;
+        }
+        token = "";
+        counter--;
+    }
+}
+if (counter == 0)
+{
+    Console.WriteLine("BALANCED");
+}
+else
+{
+    Console.WriteLine("UNBALANCED");
+}
+
