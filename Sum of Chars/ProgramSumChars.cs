2db1e54532b62b8c8da1e0d18c6a4520d8914221
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-04-19 11:36:31 +0300

    Sum of chars

---
+++ Sum of Chars/ProgramSumChars.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    char chars = char.Parse(Console.ReadLine());
+    
+    sum += (int)chars;
+
+}
+Console.WriteLine($"The sum equals: {sum}");
