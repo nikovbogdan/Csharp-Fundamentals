771ace94b627af95106a0417407fdbb5d4584155
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-04-19 11:16:30 +0300

    Elevator submit

---
+++ Elevator/ProgramElevator.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+
+if (p > n)
+{
+    Console.WriteLine("All the persons fit inside in the elevator.\nOnly one course is needed.");
+}
+else
+{
+    int courses = (int)Math.Ceiling((double)n / p);
+    Console.WriteLine(courses);
+}
+
+
